f9df02fc6b680aea93c243bc41368a9a985b4249
Andrew Cen <48923485+werdna521@users.noreply.github.com>
2020-06-06 00:11:04 +0700

    RN link

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import RootNavigator from './navigators/RootNavigator';
-import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from 'react-native';
+import { colors } from './utils/variables';
 
 const App = () => {
   return (
     <NavigationContainer>
-      <StatusBar hidden />
+      <StatusBar backgroundColor={colors.secondary} barStyle="dark-content" />
       <RootNavigator />
     </NavigationContainer>
   );
